895bcd9d4c18192ef0ee0595e864db1a42981145
Aaryan Garg <aaryan.garg1@gmail.com>
2024-01-10 14:07:53 +0530

    bug : dynamic groups for functional convs -- in channels

--- efficientvit/models/nn/flexible_ops.py
+++ efficientvit/models/nn/flexible_ops.py
@@ -103,7 +103,7 @@ class FlexibleConvLayer(nn.Module):
             bias = self.conv.bias
         out = nn.functional.conv2d(
             input, weight, bias, self.stride, self.padding,
-            self.dilation, self.groups_desc)
+            self.dilation, self.groups_desc if self.groups_desc == 1 else in_channels)
         
         # What is this exactly?
         # if getattr(FLAGS, 'conv_averaged', False):
