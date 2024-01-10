c0389766449e1c6f2c9152ba98bceed37dbeffd5
Aaryan Garg <aaryan.garg1@gmail.com>
2024-01-10 09:16:00 +0530

    bug : flexible norm import in function

--- efficientvit/models/nn/norm.py
+++ efficientvit/models/nn/norm.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 from torch.nn.modules.batchnorm import _BatchNorm
 from efficientvit.models.utils import build_kwargs_from_config
-from efficientvit.models.nn import FlexibleBatchNorm2d
 
 __all__ = ["LayerNorm2d", "build_norm", "reset_bn", "set_norm_eps", "build_flexible_norm"]
 
@@ -42,6 +41,7 @@ def build_norm(name="bn2d", num_features=None, **kwargs) -> nn.Module or None:
         return None
 
 def build_flexible_norm(num_features = None, **kwargs) -> nn.Module or None :
+    from efficientvit.models.nn import FlexibleBatchNorm2d
     norm_cls = FlexibleBatchNorm2d
     args = build_kwargs_from_config(kwargs, norm_cls)
     return norm_cls(**args)
