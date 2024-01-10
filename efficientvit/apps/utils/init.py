052fe2d06d9706506616c7e380e35aab68778eea
Aaryan Garg <aaryan.garg1@gmail.com>
2024-01-10 12:05:06 +0530

    bug : instance checking

--- efficientvit/apps/utils/init.py
+++ efficientvit/apps/utils/init.py
@@ -38,8 +38,9 @@ def init_modules(model: nn.Module or list[nn.Module], init_type="trunc_normal") 
             elif isinstance(m, FlexibleBatchNorm2d) :
                 for child_layer in m.bn.children():
                      if isinstance(child_layer, (_BatchNorm, nn.GroupNorm, nn.LayerNorm)):
-                        child_layer.weight.data.fill_(1)
-                        child_layer.bias.data.zero_()
+                         if hasattr(child_layer, 'weight') and hasattr(child_layer, 'bias'):
+                            child_layer.weight.data.fill_(1)
+                            child_layer.bias.data.zero_()
             elif isinstance(m, (_BatchNorm, nn.GroupNorm, nn.LayerNorm)):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
