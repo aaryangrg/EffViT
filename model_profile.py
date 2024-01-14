bc663b4937d8cbbba36a0494d05d9f7a6a12c0b2
Aaryan Garg <aaryan.garg1@gmail.com>
2024-01-14 19:52:48 +0530

    bug : synthetic data for thop

--- model_profile.py
+++ model_profile.py
@@ -86,7 +86,6 @@ def main():
     )
     input = torch.randn(1, 3, 224, 224)
     input.to(dtype=torch.float16)
-    input.to("cuda:0")
 
     model = create_custom_cls_model(args.student_model, False, width_multiplier = args.width_multiplier, depth_multiplier=args.depth_multiplier)
         
@@ -100,7 +99,7 @@ def main():
     with torch.inference_mode():
         with tqdm(total=len(data_loader)) as t:
             for images, labels in data_loader:
-
+                input = input.cuda()
                 # Batch recorded
                 images, labels = images.cuda(), labels.cuda()
 
