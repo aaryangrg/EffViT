2fdbe34217087902d0513f6222e8dc9c98a542f1
Aaryan Garg <aaryan.garg1@gmail.com>
2024-01-17 12:22:54 +0530

    ft : profiling only conv + mla + batchnorm

--- model_profile.py
+++ model_profile.py
@@ -58,14 +58,16 @@ def main():
 
     # # Print GPU memory summary
     # print(torch.cuda.memory_summary(device=None, abbreviated=False))
+    
     if args.profile :
         for i in range(5) :
             input = input.cuda()
             # Batch recorded
-            with profiler(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
+            with profiler(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], profile_memory=True, record_function = "model_specific") as prof:
                 model(input)
             print(prof.key_averages().table(sort_by="self_cuda_time_total"))
-        #MACS calculation & Params (single image)
+
+    #MACS calculation & Params (single image)
     # if args.find_macs : 
     #     macs, params = profile(model, inputs = (input,))
     #     print(f"MACSs: {macs}, Params: {params}")
