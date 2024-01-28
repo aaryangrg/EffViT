19c3820ad1f3e5a45891d3c3b9bd0a259b412c3d
Aaryan Garg <aaryan.garg1@gmail.com>
2024-01-28 10:38:16 +0530

    config

--- eval_cls_v1.py
+++ eval_cls_v1.py
@@ -93,7 +93,7 @@ def main():
         dloader = []
         for data in data_loader :
             dloader.append(data[0])
-        reset_bn(model=model.module,progress_bar=True, data_loader=dloader)
+        reset_bn(model=model,progress_bar=True, data_loader=dloader)
     elif args.reduced_width : 
         model = create_custom_cls_model(args.model, True, weight_url = args.weight_url, width_multiplier = args.width_multiplier, depth_multiplier=args.depth_multiplier)
     else :
