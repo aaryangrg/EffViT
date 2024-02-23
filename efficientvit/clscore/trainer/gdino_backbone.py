86e546a1b7d88293412b39d850f436f56b7af475
Aaryan Garg <aaryan.garg1@gmail.com>
2024-02-23 21:56:56 +0530

    test: dino model load

--- efficientvit/clscore/trainer/gdino_backbone.py
+++ efficientvit/clscore/trainer/gdino_backbone.py
@@ -18,7 +18,8 @@ from efficientvit.clscore.trainer.utils import accuracy, apply_mixup, label_smoo
 from efficientvit.models.utils import list_join, list_mean, torch_random_choices
 from efficientvit.apps.data_provider.base import parse_image_size
 
-# from Open-GDINO.groundingdino.misc import NestedTensor, nested_tensor_from_tensor_list
+sys.path.append('/home/aaryang/experiments/Open-GDINO/')
+from groundingdino.misc import NestedTensor, nested_tensor_from_tensor_list
 
 __all__ = ["GdinoBackboneTrainer"]
 LOG_SOFTMAX_CONST = 1e-6
@@ -107,122 +108,122 @@ class GdinoBackboneTrainer(Trainer):
         
     #     return results
 
-    # def prestep(self, samples : NestedTensor) :
+    def prestep(self, samples : NestedTensor) :
         
-    #     if isinstance(samples, (list, torch.Tensor)):
-    #         samples = nested_tensor_from_tensor_list(samples)
+        if isinstance(samples, (list, torch.Tensor)):
+            samples = nested_tensor_from_tensor_list(samples)
 
-    #     # Zero grad if any accumulates
-    #     self.optimizer.zero_grad()
+        # Zero grad if any accumulates
+        self.optimizer.zero_grad()
 
-    #     return samples
+        return samples
     
     # # Using pre-defined fixed widths (0.25, 0.50, 0.75, 1.0)
-    # def runstep(self, samples : NestedTensor) -> dict[str, any]:
+    def runstep(self, samples : NestedTensor) -> dict[str, any]:
 
-    #     # Put model to train
-    #     self.model.train()
+        # Put model to train
+        self.model.train()
 
-    #     # Use half-precision training
-    #     with torch.autocast(device_type="cuda", dtype=torch.float16, enabled=self.fp16):
-    #         dino_backbone_outputs = []
-    #         dino_backbone_features, __ = self.dino_backbone.backbone(samples)
-    #         for l, feat in enumerate(dino_backbone_features):
-    #             src, mask = feat.decompose()
-    #             dino_backbone_outputs.append(src)
-    #         with torch.no_grad() :
-    #             self.model.apply(lambda m: setattr(m, 'width_mult', PREDEFINED_WIDTHS[-1]))
+        # Use half-precision training
+        with torch.autocast(device_type="cuda", dtype=torch.float16, enabled=self.fp16):
+            dino_backbone_outputs = []
+            dino_backbone_features, __ = self.dino_backbone.backbone(samples)
+            for l, feat in enumerate(dino_backbone_features):
+                src, mask = feat.decompose()
+                dino_backbone_outputs.append(src)
+            with torch.no_grad() :
+                self.model.apply(lambda m: setattr(m, 'width_mult', PREDEFINED_WIDTHS[-1]))
 
-    #         max_width_outputs = self.model(samples) # ViT Backbone outputs - should also include masks (Feature pyramid)
+            max_width_outputs = self.model(samples) # ViT Backbone outputs - should also include masks (Feature pyramid)
 
-    #         total_kd_loss = 0
-    #         max_width_kd_loss = self.get_kld_loss(dino_backbone_outputs, max_width_outputs)
-    #         total_kd_loss += max_width_kd_loss
+            total_kd_loss = 0
+            max_width_kd_loss = self.get_kld_loss(dino_backbone_outputs, max_width_outputs)
+            total_kd_loss += max_width_kd_loss
 
-    #     self.scaler.scale(max_width_kd_loss).backward()
+        self.scaler.scale(max_width_kd_loss).backward()
         
-    #     # Bears significant computational overhead for training
-    #     for width_mult in (PREDEFINED_WIDTHS[:len(PREDEFINED_WIDTHS)-1])[::-1]:
-    #         with torch.autocast(device_type="cuda", dtype=torch.float16, enabled=self.fp16):
-    #             with torch.no_grad():
-    #                 self.model.apply(lambda m: setattr(m, 'width_mult', width_mult))
-    #             vit_outputs = self.model(samples)
-    #             kd_loss = self.get_kld_loss(vit_outputs, dino_backbone_outputs)
-    #         self.scaler.scale(kd_loss).backward()
-    #         total_kd_loss += kd_loss
+        # Bears significant computational overhead for training
+        for width_mult in (PREDEFINED_WIDTHS[:len(PREDEFINED_WIDTHS)-1])[::-1]:
+            with torch.autocast(device_type="cuda", dtype=torch.float16, enabled=self.fp16):
+                with torch.no_grad():
+                    self.model.apply(lambda m: setattr(m, 'width_mult', width_mult))
+                vit_outputs = self.model(samples)
+                kd_loss = self.get_kld_loss(vit_outputs, dino_backbone_outputs)
+            self.scaler.scale(kd_loss).backward()
+            total_kd_loss += kd_loss
     
-    #     return {
-    #         "loss": total_kd_loss,
-    #     }
+        return {
+            "loss": total_kd_loss,
+        }
     
-    # def get_kld_loss(self,scale_pred, scale_soft, temperature = 1.0):
-    #     loss = 0
-    #     for _ in range(len(scale_pred)) :
-    #         p_s = F.log_softmax(scale_pred[_] + LOG_SOFTMAX_CONST / temperature, dim=1)
-    #         p_t = F.softmax(scale_soft[_] + LOG_SOFTMAX_CONST / temperature, dim=1)
-    #         loss += F.kl_div(p_s, p_t, reduction='mean')
-    #     return loss
+    def get_kld_loss(self,scale_pred, scale_soft, temperature = 1.0):
+        loss = 0
+        for _ in range(len(scale_pred)) :
+            p_s = F.log_softmax(scale_pred[_] + LOG_SOFTMAX_CONST / temperature, dim=1)
+            p_t = F.softmax(scale_soft[_] + LOG_SOFTMAX_CONST / temperature, dim=1)
+            loss += F.kl_div(p_s, p_t, reduction='mean')
+        return loss
 
-    # def _train_one_epoch(self, epoch: int) -> dict[str, any]:
+    def _train_one_epoch(self, epoch: int) -> dict[str, any]:
     
-    #     train_loss = AverageMeter()
+        train_loss = AverageMeter()
 
-    #     with tqdm(
-    #         total=len(self.data_provider),
-    #         desc="Train Epoch #{}".format(epoch + 1),
-    #         disable=not dist.is_master(),
-    #         file=sys.stdout,
-    #     ) as t:
-    #         for samples, targets in self.data_provider:
-    #             # preprocessing
-    #             samples = self.prestep(samples)
-    #             # clear gradient
-    #             self.optimizer.zero_grad()
-    #             # forward & backward
-    #             output_dict = self.runstep(samples)
-    #             # update: optimizer, lr_scheduler
-    #             self.after_step()
+        with tqdm(
+            total=len(self.data_provider),
+            desc="Train Epoch #{}".format(epoch + 1),
+            disable=not dist.is_master(),
+            file=sys.stdout,
+        ) as t:
+            for samples, targets in self.data_provider:
+                # preprocessing
+                samples = self.prestep(samples)
+                # clear gradient
+                self.optimizer.zero_grad()
+                # forward & backward
+                output_dict = self.runstep(samples)
+                # update: optimizer, lr_scheduler
+                self.after_step()
 
-    #             # update train metrics
-    #             train_loss.update(output_dict["loss"])
+                # update train metrics
+                train_loss.update(output_dict["loss"])
 
-    #             # tqdm
-    #             postfix_dict = {
-    #                 "loss": train_loss.avg,
-    #                 "lr": list_join(
-    #                     sorted(set([group["lr"] for group in self.optimizer.param_groups])),
-    #                     "#",
-    #                     "%.1E",
-    #                 ),
-    #                 "progress": self.run_config.progress,
-    #             }
-    #             t.set_postfix(postfix_dict)
-    #             t.update()
-    #     return {
-    #         "loss" : train_loss.avg
-    #     }
+                # tqdm
+                postfix_dict = {
+                    "loss": train_loss.avg,
+                    "lr": list_join(
+                        sorted(set([group["lr"] for group in self.optimizer.param_groups])),
+                        "#",
+                        "%.1E",
+                    ),
+                    "progress": self.run_config.progress,
+                }
+                t.set_postfix(postfix_dict)
+                t.update()
+        return {
+            "loss" : train_loss.avg
+        }
 
-    # def train(self, trials=0, save_freq=1) -> None:
+    def train(self, trials=0, save_freq=1) -> None:
 
-    #     for epoch in range(self.start_epoch, self.run_config.n_epochs + self.run_config.warmup_epochs):
-    #         train_info_dict = self.train_one_epoch(epoch)
+        for epoch in range(self.start_epoch, self.run_config.n_epochs + self.run_config.warmup_epochs):
+            train_info_dict = self.train_one_epoch(epoch)
             
-    #         # log
-    #         val_log = self.run_config.epoch_format(epoch)
-    #         val_log += ")\tTrain("
-    #         for key, val in train_info_dict.items():
-    #             val_log += f"{key}={val:.2E},"
-    #         val_log += (
-    #             f'lr={list_join(sorted(set([group["lr"] for group in self.optimizer.param_groups])), "#", "%.1E")})'
-    #         )
-    #         self.write_log(val_log, prefix="valid", print_log=False)
+            # log
+            val_log = self.run_config.epoch_format(epoch)
+            val_log += ")\tTrain("
+            for key, val in train_info_dict.items():
+                val_log += f"{key}={val:.2E},"
+            val_log += (
+                f'lr={list_join(sorted(set([group["lr"] for group in self.optimizer.param_groups])), "#", "%.1E")})'
+            )
+            self.write_log(val_log, prefix="valid", print_log=False)
 
-    #         # save model
-    #         self.save_model(
-    #             only_state_dict=False,
-    #             epoch=epoch,
-    #             model_name="model_best.pt",
-    #         )
+            # save model
+            self.save_model(
+                only_state_dict=False,
+                epoch=epoch,
+                model_name="model_best.pt",
+            )
 
     # def validate(self, model=None, data_loader=None, is_test=True, epoch=0, reset_bn = False) -> dict[str, any]:
     #     model = model or self.eval_network
--- gdino_back_distill.py
+++ gdino_back_distill.py
@@ -66,9 +66,9 @@ def main():
     effvit_backbone.cuda()
 
     # Load GDINO model
-    # model, criterion, postprocessors = build_model_main(args)
-    # model.to(device)
-    # logger.debug("build model, done.")
+    model, criterion, postprocessors = build_model_main(args)
+    model.cuda()
+    print("build model, done.")
 
 
     # model_without_ddp = model
@@ -79,33 +79,33 @@ def main():
     # SET MODEL TO EVAL
     
     # Dummy input
-    dummy = torch.rand(2,3,1024,1024)
-    dummy = dummy.to("cuda")
+    # dummy = torch.rand(2,3,1024,1024)
+    # dummy = dummy.to("cuda")
 
-    effvit_backbone.eval()
-    with torch.no_grad() :
-        print("1x")
-        effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 1.0))
-        outs = effvit_backbone(dummy)
-        for i in range(len(outs)) :
-            print(outs[i].shape)
+    # effvit_backbone.eval()
+    # with torch.no_grad() :
+    #     print("1x")
+    #     effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 1.0))
+    #     outs = effvit_backbone(dummy)
+    #     for i in range(len(outs)) :
+    #         print(outs[i].shape)
 
-        print("0.75x")
-        effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 0.75))
-        outs = effvit_backbone(dummy)
-        for i in range(len(outs)) :
-            print(outs[i].shape)
+    #     print("0.75x")
+    #     effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 0.75))
+    #     outs = effvit_backbone(dummy)
+    #     for i in range(len(outs)) :
+    #         print(outs[i].shape)
 
-        print("0.50x")
-        effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 0.50))
-        outs = effvit_backbone(dummy)
-        for i in range(len(outs)) :
-            print(outs[i].shape)
-        print("0.25x")
-        effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 0.25))
-        outs = effvit_backbone(dummy)
-        for i in range(len(outs)) :
-            print(outs[i].shape)
+    #     print("0.50x")
+    #     effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 0.50))
+    #     outs = effvit_backbone(dummy)
+    #     for i in range(len(outs)) :
+    #         print(outs[i].shape)
+    #     print("0.25x")
+    #     effvit_backbone.apply(lambda m: setattr(m, 'width_mult', 0.25))
+    #     outs = effvit_backbone(dummy)
+    #     for i in range(len(outs)) :
+    #         print(outs[i].shape)
     
     # dataset_train = build_dataset(image_set='train', args=args, datasetinfo=dataset_meta["train"][0])
     # if args.distributed:
