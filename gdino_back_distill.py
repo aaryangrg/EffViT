5e9e4159118837253058845afd34b52a60ad37d2
Aaryan Garg <aaryan.garg1@gmail.com>
2024-02-24 16:15:42 +0530

    test

--- gdino_back_distill.py
+++ gdino_back_distill.py
@@ -18,10 +18,10 @@ import torch
 from torch.utils.data import DataLoader, DistributedSampler
 import json
 
-from .gdino.main import build_model_main
-from .gdino.util.slconfig import SLConfig
-from .gdino.util.misc import collate_fn
-from .gdino.datasets import bbuild_dataset
+# from gdino.main import build_model_main
+from gdino.util.slconfig import SLConfig
+from gdino.util.misc import collate_fn
+from gdino.datasets import bbuild_dataset
 
 # sys.path.append('/home/aaryang/experiments/')
 # gdino = importlib.import_module("Open-GDINO")
@@ -105,9 +105,10 @@ def main():
     effvit_backbone.cuda()
 
     # Load GDINO model
-    model, criterion, postprocessors = build_model_main(args)
-    model.cuda()
-    print("build model, done.")
+    # model, criterion, postprocessors = build_model_main(args)
+    # model.cuda()
+    # print("build model, done.")
+    model = None
 
 
     # model_without_ddp = model
