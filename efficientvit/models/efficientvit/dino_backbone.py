12ab4b3307bb61db4ae0f7b344bb4202cdd986b6
Aaryan Garg <aaryan.garg1@gmail.com>
2024-03-21 13:46:41 +0530

    ft : profiling gdino backbone for FLOPs at res and width

--- efficientvit/models/efficientvit/dino_backbone.py
+++ efficientvit/models/efficientvit/dino_backbone.py
@@ -254,11 +254,7 @@ def flexible_efficientvit_backbone_swin_t_224_1k(**kwargs) -> FlexibleGDINOBackb
     backbone = FlexibleGDINOBackbone(
         # width_list=[16, 96, 192, 384, 768],
         width_list = [32, 64, 128, 256, 512],
-        # depth_list=[1, 2, 3, 3, 4],
-        # depth_list=[1,2,2,2,3],
-        # depth_list = [1,2,2,8,8],
         depth_list = [1, 4, 6, 6, 9],
-        # dim = 16,
         dim=32,
         **build_kwargs_from_config(kwargs, FlexibleGDINOBackbone),
     )
