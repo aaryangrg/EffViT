84be56259948139e0cce13db92aa3083c5b6520c
Aaryan Garg <aaryan.garg1@gmail.com>
2024-04-01 09:01:36 +0530

    rev : task in fp16

--- efficientvit/clscore/trainer/dino_flexless.py
+++ efficientvit/clscore/trainer/dino_flexless.py
@@ -148,14 +148,10 @@ class GdinoBackboneTrainerNoFlex(Trainer):
             loss_dict = self.task_criterion(final_outputs, targets, cap_list, captions)
             weight_dict = self.task_criterion.weight_dict
             task_losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
-            # self.scaler.scale(task_losses).backward()
-        # with torch.autocast(device_type="cuda", dtype=torch.float16, enabled=self.fp16_training):
-            max_width_kd_loss = self.loss_criterion(backbone_outputs, dino_backbone_outputs)
-            # max_width_kd_loss = self.get_kld_loss_single(backbone_outputs[0], dino_backbone_outputs[0])
+            # max_width_kd_loss = self.loss_criterion(backbone_outputs, dino_backbone_outputs)
+            max_width_kd_loss = 0
             total_loss = task_losses + max_width_kd_loss
-            self.scaler.scale(total_loss).backward() # Back prop distillation loss
-        # total_loss = task_losses + max_width_kd_loss 
-        # self.scaler.scale(total_loss).backward()
+            self.scaler.scale(total_loss).backward()
 
         return {
             "kd_loss": max_width_kd_loss,
